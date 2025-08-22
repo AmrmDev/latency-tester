b5017f7c1c5e2d9a0e3e26287a066dd79be85649
Pedro Amorim <amorimpedro23@outlook.com>
2025-08-21 22:19:30 -0300

    host TCP connection latency created

--- internal/infrastructure/net/http_latency.go
+++ internal/infrastructure/net/http_latency.go
@@ -1,1 +1,33 @@
-package net
+package netinfra
+
+import (
+	"net"
+	"time"
+
+	"github.com/AmrmDev/latency-tester/internal/domain"
+)
+
+type HttpLatencyTester struct{}
+
+func NewHttpLatencyTester() *HttpLatencyTester {
+	return &HttpLatencyTester{}
+}
+
+func (t *HttpLatencyTester) Run(host string) (domain.LatencyTest, error) {
+	start := time.Now()
+
+	conn, err := net.DialTimeout("tcp", host+":80", 3*time.Second)
+	if err != nil {
+		return domain.LatencyTest{}, err
+	}
+	defer conn.Close()
+
+	duration := time.Since(start)
+
+	return domain.LatencyTest{
+		Host: host,
+		Duration:  duration,
+		Timestamp: time.Now(),
+	}, nil
+
+}
