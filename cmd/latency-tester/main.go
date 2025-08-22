73dd387f8f4f33c618ae5b7334e05d56ff621ebb
Pedro Amorim <amorimpedro23@outlook.com>
2025-08-21 22:47:28 -0300

    main.go to run the application

--- cmd/latency-tester/main.go
+++ cmd/latency-tester/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/AmrmDev/latency-tester/internal/adapter/cli"
+	"github.com/AmrmDev/latency-tester/internal/infrastructure/net"
+	"github.com/AmrmDev/latency-tester/internal/usecase"
+)
+
+func main() {
+	tester := netinfra.NewHttpLatencyTester()
+	usecase := usecase.NewLatencyTesterUseCase(tester)
+
+	http.HandleFunc("/latency", handler.LatencyHandler(usecase))
+
+	fmt.Println("Latency Tester API is running on port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Failed to start server: %v\n", err)
+	}
+}
