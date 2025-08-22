1544395b5beb816135d5d80c12b3aefd765837c1
Pedro Amorim <amorimpedro23@outlook.com>
2025-08-21 22:23:07 -0300

    added usecase for the latency tester

--- internal/usecase/latency_tester_impl_usecase.go
+++ internal/usecase/latency_tester_impl_usecase.go
@@ -1,1 +1,17 @@
 package usecase
+
+import (
+	"github.com/AmrmDev/latency-tester/internal/domain"
+)
+
+type LatencyTesterUseCase struct {
+	Tester domain.LatencyTester
+}
+
+func NewLatencyTesterUseCase(tester domain.LatencyTester) *LatencyTesterUseCase {
+	return &LatencyTesterUseCase{Tester: tester}
+}
+
+func (uc *LatencyTesterUseCase) Execute (host string) (domain.LatencyTest, error) {
+	return uc.Tester.Run(host)
+}
